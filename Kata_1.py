73fd1cfe1103ed2ede199cb00563fff4e182426e
Marcel Walk <walk.marcel.97@googlemail.com>
2021-09-08 08:05:52 +0200

    Day 1

---
+++ Kata_1.py
@@ -0,0 +1,24 @@
+"""
+Write a function that takes in a string of one or more words, and returns the same string,
+but with all five or more letter words reversed (like the name of this kata).
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+"""
+
+
+def spin_words(sentence):
+
+    words = sentence.split()
+    result = []
+
+    for word in words:
+        if len(word) >= 5:
+            result.append(word[::-1])
+        else:
+            result.append(word)
+
+    return " ".join(result)
+
+
+def spin_words_sol(sentence):
+    return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
